c0cb5dcd6eee5b807bea2768fae934d168a1e8e7
rangasnafu - Home <135566054+rangasnafu@users.noreply.github.com>
2023-10-05 23:00:28 +1100

    complete confirmation + remove text raycast target

--- Assets/Scripts/Confirmation.cs
+++ Assets/Scripts/Confirmation.cs
@@ -7,15 +7,16 @@ public class Confirmation : MonoBehaviour
 {
     public GameObject yesButton;
     public GameObject noButton;
+    public GameObject confirmationUI;
 
     public void Yes()
     {
-
+        SceneManager.LoadScene("Questions");
     }
 
     public void No()
     {
-
+        confirmationUI.SetActive(false);
     }
 
 }
